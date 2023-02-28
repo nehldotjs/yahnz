fb71edf5d4943745c23dddb0ed66553da30befb5
nehldotjs <nelsonosuya11@gmail.com>
2023-02-28 17:18:40 +0100

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App" basename="/yahnz">
-      <h1>WELCOME TO YAHNZ Official website, Sorry Website is currently Under construction, Thank you please check back later</h1>
-    </div>
-  );
-}
-
-export default App;
